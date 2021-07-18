e0b2e418ab9d7eb1346b32601e04289de9db7e39
Lugas Gilas Angkoro <lugasgilas@gmail.com>
2021-07-18 14:22:57 +0700

    Update filter
    Update filter

---
+++ assets/js/filter.js
@@ -0,0 +1,9 @@
+
+function openFilters() {
+  document.getElementById("filtersProduct").style.width = "350px"; 
+}
+
+function closeFilters() {
+  document.getElementById("filtersProduct").style.width = "0"; 
+}
+   
